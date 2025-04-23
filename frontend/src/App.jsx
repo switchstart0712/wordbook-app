23834ed706b36363204380ba03a39a1dfb2e1c96
switchstart0712 <switchstart0712@gmail.com>
2025-04-24 08:55:00 +0900

    UI表示テスト

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,72 @@
 import React from 'react';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 入力フォーム用ステート
+  const[word, setWord] = useState("");
+  const[meaning, setMeaning] = useState("");
+
+  // 単語一覧ステート
+  const [words, setWords] = useState([]);
+
+  // 一覧取得(初回マウント時にuseEffect()でGET/wordsを実行)
+  useEffect(() => {
+    fetchWords();
+  }, []);
+
+  // GETリクエスト
+  const fetchWords = async(e) => {
+    const res = await fetch("http://localhost:8000/words");
+    const data = await res.json();
+    setWords(data);
+  };
+
+  // POSTリクエスト
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch("http://localhost:8000/words", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ word, meaning }),
+    });
+
+    // 入力欄クリア & 一覧更新
+    setWord("");
+    setMeaning("");
+    fetchWords();
+  };
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>英単語帳</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="英単語"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+          <input
+            placeholder="意味"
+            value={meaning}
+            onChange={(e) => setMeaning(e.target.value)}
+          />
+          <button type="submit">登録</button>
+        </form>
+
+        {/* words配列に格納されている全ての単語を<ul>で表示 */}
+        <ul>
+          {words.map((w) => (
+            <li key={w.id}>
+              {w.word} - {w.meaning}
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
